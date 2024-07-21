9cf1b8a0f5a71f89762be6e5d19200e3d1d15000
Alex <ayruthe@gmail.com>
2024-07-21 15:15:50 -0400

    filter by question marks and length

--- questions/language.py
+++ questions/language.py
@@ -12,6 +12,8 @@ def nlp_preproc(file: Path) -> None:
     This function serves as a basic pipeline for tokenization, 
     part-of-speech (POS) tagging, lemmatization, and name-entity 
     recognition (NER) built with SpaCy library. 
+
+    # TODO: Filter out patterns: '.music', 'MIDROLL', '. music'
     
     Args:
         file: the json file path to transcript data to preprocess. 
@@ -23,6 +25,58 @@ def nlp_preproc(file: Path) -> None:
     json_data = sentiment_analysis(json_data)
     save_json(file, json_data)
 
+    # Make LLM Training JSON with (Prev Answer, Next Question) Format
+    train_json = format_for_training(json_data)
+    train_json = filter_by_question_mark(train_json)
+    train_json = filter_by_length(train_json)
+    savefile = file.parent.joinpath('training_data.json')
+    save_json(savefile, train_json)
+
+
+def filter_by_length(json_data: dict, str_length_min: int = 30) -> dict:
+    """Preprocessing step to label sentiment of sentences.
+
+    Args:
+        json_data: dictionary transcript data to process.
+    """
+    new_json = {'prev_answer':[], 'next_question':[], 'prev_answer_sentiment':[]}
+    for index in range(len(json_data['prev_answer'])):
+        if (len(json_data['next_question'][index]) > str_length_min) and (len(json_data['prev_answer'][index]) > str_length_min):
+            new_json['next_question'].append(json_data['next_question'][index])
+            new_json['prev_answer'].append(json_data['prev_answer'][index])
+            new_json['prev_answer_sentiment'].append(json_data['prev_answer_sentiment'][index])
+    return new_json
+
+
+def filter_by_question_mark(json_data: dict) -> dict:
+    """Preprocessing step to label sentiment of sentences.
+
+    Args:
+        json_data: dictionary transcript data to process.
+    """
+    questions_json = {'prev_answer':[], 'next_question':[], 'prev_answer_sentiment':[]}
+    for index in range(len(json_data['prev_answer'])):
+        if '?' in json_data['next_question'][index]:
+            questions_json['next_question'].append(json_data['next_question'][index])
+            questions_json['prev_answer'].append(json_data['prev_answer'][index])
+            questions_json['prev_answer_sentiment'].append(json_data['prev_answer_sentiment'][index])
+    return questions_json
+
+
+def format_for_training(json_data: dict) -> dict:
+    """Preprocessing step to label sentiment of sentences.
+
+    Args:
+        json_data: dictionary transcript data to process.
+    """
+    train_json = {
+        'prev_answer' : json_data['answers'][:-1],
+        'prev_answer_sentiment' : json_data['a_sentiment'][:-1],
+        'next_question' : json_data['questions'][1:],
+        }
+    return train_json
+    
+
     
 def sentiment_analysis(json_data: dict) -> dict:
     """Preprocessing step to label sentiment of sentences.
@@ -33,7 +87,6 @@ def sentiment_analysis(json_data: dict) -> dict:
     n_qa = len(json_data['questions'])
     q_sentiment = []
     a_sentiment = []
-    nlp = spacy.load("en_core_web_sm")
     for qa_str in ['questions', 'answers']:
         for idx in range(n_qa):
             text = json_data[qa_str][idx]

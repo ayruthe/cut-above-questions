234830ad2d1b934ebdb3e79b6e49d9ef58488c6a
Alex <ayruthe@gmail.com>
2024-07-22 10:07:34 -0400

    load model in run script and make model available

--- scripts/run.py
+++ scripts/run.py
@@ -18,23 +18,26 @@ if __name__ == "__main__":
     metadata_savefile = output_filepath.joinpath('transcript_metadata.json')   
     
     # 2. Preprocess
-    TransciptDataset(input_filepath, data_savefile, metadata_savefile)
-    nlp_preproc(data_savefile)
+    # TransciptDataset(input_filepath, data_savefile, metadata_savefile)
+    # nlp_preproc(data_savefile)
     
-    # 3. NER and Sentiment Analysis
-    interview_named_entity_analysis(data_savefile)
+    # # 3. NER and Sentiment Analysis
+    # interview_named_entity_analysis(data_savefile)
 
     # 4. Train LLM
-    lora_rank = 4
-    dataset_subsample_rate = 200
+    lora_rank = 32
+    dataset_subsample_rate = 1
     model_name = 'google/flan-t5-base'
     result_path = Path().absolute().joinpath(f'results/training-lora-rank-{lora_rank}-subsample-{dataset_subsample_rate}')
-    train_file = output_filepath.joinpath('training_data.json') 
-    llm = InterviewQuestionLLM(train_file, result_path, lora_rank, dataset_subsample_rate, model_name)
-    llm.zeroshot_test()
-    llm.low_rank_adaptation_training()
+    model_path = result_path.joinpath('peft-checkpoint-local')
+    train_file = output_filepath.joinpath('training_data.json')
+    if not model_path.joinpath('adapter_model.bin').is_file():
+        llm = InterviewQuestionLLM(train_file, result_path, lora_rank, dataset_subsample_rate, model_name)
+        llm.zeroshot_test()
+        llm.low_rank_adaptation_training()
+    else:
+        print('Loading Model')
 
     # 5. Evaluate LLM
-    model_path = result_path.joinpath('peft-checkpoint-local')
     llm = InterviewQuestionLLM(train_file, result_path, lora_rank, dataset_subsample_rate, model_name)
     llm.evaluate_llm(train_file, model_path)
